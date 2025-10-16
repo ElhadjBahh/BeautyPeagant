729ee9f86776c87bd55f63e5a23da7c654a11525
Elhadj Bah <issiagabahh1997@icloud.com>
2025-10-16 17:10:53 -0600

    Version one

---
+++ src/Contestants.java
@@ -0,0 +1,30 @@
+public class Contestants {
+
+    String divaName;
+    int scoreCard;
+
+    public Contestants (String name ) {
+
+        this.divaName = name;
+        this.scoreCard = scoreCard;
+
+
+    }
+
+    public void setDivaName(String divaName) {
+        this.divaName = divaName;
+    }
+
+    public void setScoreCard(int scoreCard) {
+        this.scoreCard = scoreCard;
+    }
+
+    public int getScoreCard() {
+        return scoreCard;
+    }
+
+    public String getDivaName() {
+        return divaName;
+    }
+
+}
--- src/main.java
+++ src/main.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String [] args) {
 
-        constestant divaname = new constestant("Asseya", 3);
-        divaname.print();
+        Scanner keybroad = new Scanner(System.in);
+
+
+        int only5Diva = 5;
+
+    while (only5Diva <=5) {
+
+        System.out.println("Enter your name: ");
+
+        Contestants diva1 = new Contestants()
+
+
+        only5Diva++;
+    }
+
+    }
+
+    public static void judges () {
+
+
+        int judgeInput;
+
+        System.out.println("Enter your score for " );
 
     }
 }
